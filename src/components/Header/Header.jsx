16bc99eacd8fd5a41f4c55659bccc4bda3c3e80d
luisfernando <luisfernandomsmg@gmail.com>
2026-05-04 11:13:23 -0600

    vemos2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,6 @@
 import "./Header.css";
 
 function Header() {
-  const selectPage = ({ isActive }) =>
-    "header_contact" + (isActive ? " header_contact_active" : "");
   return (
     <div className="header">
       <h1 className="header__text">GAD DERECHO 2026</h1>
