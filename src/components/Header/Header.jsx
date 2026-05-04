59e77889496e575656d3348e279e7c2ba8f7ce09
luisfernando <luisfernandomsmg@gmail.com>
2026-05-04 15:13:51 -0600

    contador listo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,9 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      <h1 className="header__text">GAD DERECHO 2026</h1>
+      <h1 className="header__text">GRAD</h1>
+      <h1 className="header__text-red">DERECHO</h1>
+      <h1 className="header__text">2026</h1>
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,61 @@
+import { useState, useEffect } from "react";
 import "./Main.css";
+
 function Main() {
-  return <div className="main">coming soon</div>;
+  const targetDate = new Date("2026-05-29T14:00:00").getTime();
+
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = new Date().getTime();
+      const difference = targetDate - now;
+
+      if (difference <= 0) {
+        clearInterval(timer);
+      } else {
+        setTimeLeft({
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((difference / 1000 / 60) % 60),
+          seconds: Math.floor((difference / 1000) % 60),
+        });
+      }
+    }, 1000);
+
+    return () => clearInterval(timer); // Limpieza al desmontar
+  }, [targetDate]);
+
+  return (
+    <div className="main">
+      <div className="countdown">
+        <h1>Próximamente</h1>
+        <div className="countdown-container">
+          <div className="time-box">
+            <span>{timeLeft.days}</span>
+            <p>Días</p>
+          </div>
+          <div className="time-box">
+            <span>{timeLeft.hours}</span>
+            <p>Horas</p>
+          </div>
+          <div className="time-box">
+            <span>{timeLeft.minutes}</span>
+            <p>Minutos</p>
+          </div>
+          <div className="time-box">
+            <span>{timeLeft.seconds}</span>
+            <p>Segundos</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
