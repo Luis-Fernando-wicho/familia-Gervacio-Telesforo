8d44e3105dace85d96268d652cb667e1f704859f
luisfernando <luisfernandomsmg@gmail.com>
2026-05-04 11:18:34 -0600

    vemos

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import Main from "./components/Main/Main.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 import "./App.css";
+
 function App() {
   return (
-    <>
-      <div className="page">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    </>
+    <div className="page">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   );
 }
 
