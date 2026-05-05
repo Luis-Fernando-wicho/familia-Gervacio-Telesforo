4a08e174470f8dd5ac484340ef4588ce1f08ff19
luisfernando <luisfernandomsmg@gmail.com>
2026-05-04 19:49:10 -0600

    check de pulxeras listo para ese dia

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Main from "./components/Main/Main.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const abc = (def) => {};
   return (
     <div className="page">
       <Header />
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,12 +11,16 @@ function Main() {
     seconds: 0,
   });
 
+  // Estado para saber si el tiempo terminó
+  const [isExpired, setIsExpired] = useState(false);
+
   useEffect(() => {
     const timer = setInterval(() => {
       const now = new Date().getTime();
       const difference = targetDate - now;
 
       if (difference <= 0) {
+        setIsExpired(true);
         clearInterval(timer);
       } else {
         setTimeLeft({
@@ -28,32 +32,51 @@ function Main() {
       }
     }, 1000);
 
-    return () => clearInterval(timer); // Limpieza al desmontar
+    return () => clearInterval(timer);
   }, [targetDate]);
 
   return (
     <div className="main">
-      <div className="countdown">
-        <h1>COMING SOON</h1>
-        <div className="countdown-container">
-          <div className="time-box">
-            <span>{timeLeft.days}</span>
-            <p>Días</p>
-          </div>
-          <div className="time-box">
-            <span>{timeLeft.hours}</span>
-            <p>Horas</p>
-          </div>
-          <div className="time-box">
-            <span>{timeLeft.minutes}</span>
-            <p>Minutos</p>
-          </div>
-          <div className="time-box">
-            <span>{timeLeft.seconds}</span>
-            <p>Segundos</p>
+      {isExpired ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="400"
+          height="400"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="icon icon-tabler icons-tabler-outline icon-tabler-circle-check"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+          <path d="M9 12l2 2l4 -4" />
+        </svg>
+      ) : (
+        <div className="countdown">
+          <h1>COMING SOON</h1>
+          <div className="countdown-container">
+            <div className="time-box">
+              <span>{timeLeft.days}</span>
+              <p>Días</p>
+            </div>
+            <div className="time-box">
+              <span>{timeLeft.hours}</span>
+              <p>Horas</p>
+            </div>
+            <div className="time-box">
+              <span>{timeLeft.minutes}</span>
+              <p>Minutos</p>
+            </div>
+            <div className="time-box">
+              <span>{timeLeft.seconds}</span>
+              <p>Segundos</p>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
